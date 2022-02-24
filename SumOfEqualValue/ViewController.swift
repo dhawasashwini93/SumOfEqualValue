60c9425e593598d34a8b776b3c6149b5974d7d95
ashwini <ashwini.d@healthmeterservices.in>
2022-02-24 15:56:04 +0530

    Add values if number equal

--- SumOfEqualValue/ViewController.swift
+++ SumOfEqualValue/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var num1 : Int?
+    var num2 : Int?
+    var totalAmount : Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        SumOfEqualValue(num1: 10, num2: 20)
     }
+    
+    //MARK: - Sum Of Equal Value
 
-
+    func SumOfEqualValue(num1:Int,num2:Int) {
+    
+        if num1 != num2 {
+         
+            totalAmount = num1 * 3
+            print("Total Amount of Two Same Number \(String(describing: totalAmount))")
+            
+        } else {
+            totalAmount = num1 + num2
+            print("Total Amount of Two integers = \(String(describing: totalAmount))")
+        }
+    }
+    
 }
 
